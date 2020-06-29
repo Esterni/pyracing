66e4c49917d6a4ea33ab78f93f4d11f7e34cfbc6
esterni <Jaander@wvi.com>
2020-06-28 17:48:36 -0700

    added package detection to setup.py to remove relative importing. (re-install with pip required)

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(name='ir_webstats_rc',
+setuptools.setup(
+      name='ir_webstats_rc',
       version='1.3.2',
-      description='A version of ir_webstats hacked on by Rob Crouch',
-      url='http://github.com/fuzzwah/ir_webstats_rc',
       author='Rob Crouch',
       author_email='rob.crouch@gmail.com',
-      license='MIT',
-      packages=['ir_webstats_rc'],
+      description='A version of ir_webstats hacked on by Rob Crouch',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/fuzzwah/ir_webstats_rc',
+      packages=setuptools.find_packages(),
+      python_requires='>=3.6',
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
       zip_safe=False)
