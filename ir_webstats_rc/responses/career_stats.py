282a4ef15618928d54e41b84d04cf8e838ffb7a0
Xander <xanderriga@live.com>
2020-07-08 20:03:59 -0700

    Add responses module
    This will house the objects that we use to wrap the responses from the
    API.

---
+++ ir_webstats_rc/responses/career_stats.py
@@ -0,0 +1,17 @@
+class CareerStats:
+    def __init__(self, dict):
+        self.wins = dict['wins']
+        self.winPerc = round(dict['winPerc'], 2)
+        self.poles = dict['poles']
+        self.totalclubpoints = dict['totalclubpoints']
+        self.avgStart = round(dict['avgStart'], 2)
+        self.avgFinish = round(dict['avgFinish'], 2)
+        self.top5Perc = round(dict['top5Perc'], 2)
+        self.totalLaps = dict['totalLaps']
+        self.avgIncPerRace = round(dict['avgIncPerRace'], 2)
+        self.avgPtsPerRace = round(dict['avgPtsPerRace'], 2)
+        self.lapsLed = dict['lapsLed']
+        self.top5 = dict['top5']
+        self.lapsLedPerc = round(dict['lapsLedPerc'], 2)
+        self.category = dict['category'].replace('+', ' ')
+        self.starts = dict['starts']
