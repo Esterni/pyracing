7a023dd0d66ca73bdc2afad70037d8b6d35b8e25
Jacob Anderson <>
2020-07-12 03:18:21 -0700

    Added docstrings for all class methods explaining their functionality.

--- pyracing/client.py
+++ pyracing/client.py
@@ -122,8 +122,9 @@ class Client:
         include_empty=n,
         custID=None
     ):
-        """ Returns session information for all currently active 'open
-        practice' sessions. Default is to exclude empty sessions
+        """ Returns session information for all 'open practice' sessions that
+        are currently active. By default this only includes sessions with
+        registered drivers.
         """
         url = ct.URL_ACTIVEOP_COUNT
         payload = {
@@ -134,18 +135,27 @@ class Client:
         }
         return await self.build_request(url, payload)
 
-    async def all_subsessions(self, subSessID):
-        payload = {'subsessionid': subSessID}
+    async def all_subsessions(self, sub_sess_id):
+        """ If the given SubSessionID is one of many race splits, this
+        returns the SubSessionID for each additional split.
+        """
+        payload = {'subsessionid': sub_sess_id}
         url = ct.URL_ALL_SUBSESSIONS
         return await self.build_request(url, payload)
 
     async def car_class_by_id(self, carClassID):
+        """ Returns the CarClass data for the associated carClassID
+
+        Use id "0" for a "master" CarClass containing a list of CarClass
+        names and their respective carClassID.
+        """
         payload = {'carclassid': carClassID}
         url = ct.URL_CAREER_STATS
         return await self.build_request(url, payload)
 
     async def career_stats(self, custID):
-        """Returns driver career stats
+        """ Returns driver career stats as seen on the driver profile page.\n
+        E.g. Starts, Avg Inc., Win %, etc.
         """
         payload = {'custid': custID}
         url = ct.URL_CAREER_STATS
@@ -178,7 +188,16 @@ class Client:
             series_lic_group_id=False,
             car_id=False
     ):
-        """Returns data about all seasons."""
+        """ Returns data about all SeasonIDs. A SeasonID is unique to a series
+        for a specific year:quarter, so in 1 year a series will have gone
+        through 4 different SeasonIDs.
+
+        only_active is a flag to return results of only the currently running
+        series. The remaining kwargs change which fields are returned for each
+        SeasonID. Setting only 'category' and 'only_active' to True will give
+        you *only* a list of items with a single number between 1 and 4
+        indicating Road, Oval, Dirt Road, Dirt Oval, respectively.
+        """
         # This is a single query string; Setting to False removes it.
         field_dict = {
             'year': year,
@@ -240,6 +259,13 @@ class Client:
                            order=ct.Sort.descending,
                            active=1
                            ):
+        """ Returns a list of drivers that match the given parameters.
+        This is the backend source for /DriverLookup.Do AKA 'Driver Stats.'
+
+        This function can be used to search drivers by name using the search=
+        field and entering their full driver name. custid is only used for
+        showing where you are in relation to the list of drivers when using
+        the main /DriverLookup page on iRacing.  """
         payload = {
             'search': str(search),
             'friend': friend,
@@ -283,7 +309,7 @@ class Client:
         sort=ct.Sort.session_name,
         order=ct.Sort.ascending
     ):
-        """Returns a list of private sessions that the driver
+        """ Returns private sessions that the driver
         has participated in.
         """
         payload = {
@@ -299,7 +325,8 @@ class Client:
         return await self.build_request(url, payload)
 
     async def last_race_stats(self, custID):
-        """Returns stat summary for the drivers last 10 races
+        """ Returns stat summary for the driver's last 10 races as seen
+        on the /CareerStats page.
         """
         payload = {'custid': custID}
         url = ct.URL_LASTRACE_STATS
@@ -311,29 +338,34 @@ class Client:
         return list(map(lambda x: LastRaceStats(x), response.json()))
 
     async def last_series(self, custID):
-        """Returns a summary of stats about a drivers last 3 series.
+        """ Returns a summary of stats about a driver's last 3 series as seen
+        on the /CareerStats page.
         """
         payload = {'custid': custID}
         url = ct.URL_LAST_SERIES
         return await self.build_request(url, payload)
 
     async def member_cars_driven(self, custID):
-        """Returns which cars a driver has driven as carID.
+        """ Returns which cars (carID) someone has driven.
         """
         payload = {'custid': custID}
         url = ct.URL_CARS_DRIVEN
         return await self.build_request(url, payload)
 
     async def member_division(self, seasonID, custID):
-        """Returns the drivers division from a seasonid
+        """ Returns which division the driver was in for the
+        specified seasonID.
         """
-        payload = {'seasonid': seasonID,
-                   'custid': custID, 'pointstype': 'race'}
+        payload = {
+            'seasonid': seasonID,
+            'custid': custID,
+            'pointstype': 'race'
+            }
         url = ct.URL_MEM_DIVISION
         return await self.build_request(url, payload)
 
     async def member_sub_id_from_session(self, sessNum, custID):
-        """Returns which SubSession ID that a member was
+        """ Returns which SubSession ID that a member was
         in from a given Session ID.
         """
         payload = {'custid': custID, 'sessionID': sessNum}
@@ -343,6 +375,7 @@ class Client:
     # Might not be useful. Must be logged in and not affected by custID.
 
     async def my_racers(self, friends=1, studied=1, blacklisted=1):
+        """ Not useful. Returns only friendslist for the person logged in """
         payload = {
             'friends': friends,
             'studied': studied,
@@ -351,9 +384,14 @@ class Client:
         url = ct.URL_MY_RACERS
         return await self.build_request(url, payload)
 
-    async def next_event(self, seriesID, event=ct.EventType.race):
-        """Returns information for the upcoming session with given
-        seriesID, evtType, and date.
+    async def next_event(
+        self,
+        series_id,
+        event_type=ct.EventType.race,
+        date=ct.now_unix_ms
+    ):
+        """ Returns information about the next event (from the requested time)
+        for the series_id.
         """
         payload = {
             'seriesID': seriesID,
@@ -364,7 +402,8 @@ class Client:
         return await self.build_request(url, payload)
 
     async def personal_bests(self, carID, custID):
-        """Returns the drivers best laptimes
+        """ Returns the drivers best laptimes for the given carID, as seen on
+        the /CareerStats page.
         """
         payload = {'custid': custID, 'carid': carID}
         url = ct.URL_PERSONAL_BESTS
@@ -393,6 +432,10 @@ class Client:
         hide_ineligible=None,
         show_official=None
     ):
+        """ Returns all data used by the race guide page for the active
+        seasons. Filters are identical to those found when visiting the
+        race guide with a browser.
+        """
         payload = {
             'at': unix_time,
             'showRookie': show_rookie,
@@ -416,15 +459,28 @@ class Client:
         url = ct.URL_RACEGUIDE
         return await self.build_request(url, payload)
 
-    async def race_laps_all(self, subSessID, carClassID=-1):
-        payload = {'subsessionid': subSessID, 'carclassid': carClassID}
+    async def race_laps_all(self, sub_sess_id, carClassID=-1):
+        """ Returns information about all laps of a race for *every*
+        driver. The class of car can be set for multiclass races.
+
+        To specify laps of a single driver, use race_laps_driver().
+        """
+        payload = {'subsessionid': sub_sess_id, 'carclassid': carClassID}
         url = ct.URL_LAPS_ALL
         return await self.build_request(url, payload)
 
-    async def race_laps_driver(self, subSessID, simSessID, custID):
+    async def race_laps_driver(
+        self,
+        custID,
+        sub_sess_id,
+        sim_sess_id=ct.SimSesNum.Race
+    ):
+        """ Returns data for all laps completed of a single driver.
+        sim_sess_id specifies the laps from practice, qual, or race.
+        """
         payload = {
-            'subsessionid': subSessID,
-            'simsessnum': simSessID,
+            'subsessionid': sub_sess_id,
+            'simsessnum': sim_sess_id,
             'groupid': custID
         }
         url = ct.URL_LAPS_SINGLE
@@ -464,6 +520,9 @@ class Client:
         car_class=-1,
         car_id=-1
     ):
+        """ Returns all data about the results of a session. Providing a
+        custID allows for returning all results by a specific driver.
+        """
         payload = {
             'custID': custID,
             'showraces': show_races,
@@ -499,7 +558,8 @@ class Client:
         return await self.build_request(url, payload)
 
     async def season_for_session(self, sessionID):
-        """Returns the seasonID for a given sessionID
+        """ Returns the seasonID for a given sessionID. That is this endpoints
+        only purpose.
         """
         payload = {'sessionID': sessionID}
         url = ct.URL_SEASON_FOR_SESSION
@@ -517,6 +577,9 @@ class Client:
             sort=ct.Sort.champ_points,
             order=ct.Sort.descending
     ):
+        """ Returns the championship point standings of a series.
+        This is the same data found in /statsseries.jsp
+        """
 
         payload = {
             'seasonid': seasonID,
@@ -533,35 +596,59 @@ class Client:
         return await self.build_request(url, payload)
 
     async def series_race_results(self, seasonID, raceWeek=-1):
+        """ Returns the race results for a seasonID. raceWeek can be
+        specified to reduce the size of data returned.
+        """
         payload = {'seasonid': seasonID, 'raceweek': raceWeek}
         url = ct.URL_SERIES_RACERESULTS
         return await self.build_request(url, payload)
 
     async def session_times(self, seasonID):
+        """ Returns the next 5 sessions and their starting times from a
+        seasonID.
+        """
         payload = {'season': seasonID}
         url = ct.URL_SESSION_TIMES
         return await self.build_request(url, payload)
 
     async def get_irating(self, category, custID):
+        """ Utilizes the stats_chart class to return a list of iRating values
+        that are used in the /CareerStats charts. Accessing
+        get_irating().current will give the most recent irating of a custID
+        """
         chart_type = ct.ChartType.irating
         response = await self.stats_chart(category, custID, chart_type)
         irating_list = list(map(lambda x: IRating(x), response.json()))
+
         return ChartData(category, ct.ChartType.irating, irating_list)
 
     async def get_ttrating(self, category, custID):
+        """ Utilizes the stats_chart class to return a list of ttrating values
+        that are used in the /CareerStats charts.
+        """
         chart_type = ct.ChartType.ttrating
         response = await self.stats_chart(category, custID, chart_type)
         ttrating_list = list(map(lambda x: TTRating(x), response.json()))
+
         return ChartData(category, chart_type, ttrating_list)
 
     async def get_license_class(self, category, custID):
+        """ Utilizes the stats_chart class to return a list of license values
+        that are used in the /CareerStats charts. See the LicenseClass class
+        for how to further use this data.
+        """
         chart_type = ct.ChartType.license_class
         response = await self.stats_chart(category, custID, chart_type)
         license_class_list = list(
             map(lambda x: LicenseClass(x), response.json()))
+
         return ChartData(category, chart_type, license_class_list)
 
     async def stats_chart(self, category, custID, chartType):
+        """ Returns a list in the form of time:value for the race category
+        specified. chartType changes values between iRating, ttRating, or
+        Safety Rating. Category chooses 1 of the 4 disciplines.
+        """
         payload = {
             'custId': custID,
             'catId': category,
@@ -570,9 +657,13 @@ class Client:
         url = ct.URL_STATS_CHART
         return await self.build_request(url, payload)
 
-    async def sub_sess_results(self, subSessID, custID):
+    # TODO New name to differentiate "event" results from session data (this)
+    async def sub_sess_results(self, sub_sess_id, custID):
+        """ Returns extensive data about a session. This endpoint contains
+        data points about a session that are unavaible anywhere else.
+        """
         payload = {
-            'subsessionID': subSessID,
+            'subsessionID': sub_sess_id,
             'custid': custID
         }
         url = ct.URL_SUBS_RESULTS
@@ -585,6 +676,8 @@ class Client:
         car_id=-1,
         race_week=-1
     ):
+        """ Returns championship point standings of Teams.
+        """
         payload = {
             'raceWeekNum': race_week,
             'seasonid': season_id,
@@ -594,19 +687,22 @@ class Client:
         url = ct.URL_SUBS_RESULTS
         return await self.build_request(url, payload)
 
-    async def ticker_sessions(self):
-        payload = {}
-        url = ct.URL_TICKER_SESSIONS
-        return await self.build_request(url, payload)
-
     # TODO Does not return JSON format. Find how to convert.
 
     async def total_registered_all(self):
+        """ Returns a list of every upcoming session and the number of
+        drivers that have registered. This data is used in the small text
+        next to each series name in /Series.do that shows number of registered
+        drivers for that series.
+        """
         payload = {}
         url = ct.URL_TOTALREGISTERED
         return await self.build_request(url, payload)
 
     async def world_records(self, year, quarter, carID, trackID, custID):
+        """ Returns laptimes with the requested paramaters. Filters can also
+        be seen on the /worldrecords.jsp page on the membersite.
+        """
         payload = {
             'seasonyear': year,
             'seasonquarter': quarter,
@@ -620,6 +716,9 @@ class Client:
         return await self.build_request(url, payload)
 
     async def yearly_stats(self, custID):
+        """ Returns the breakdown of career stats by year, as seen on the
+        /CareerStats driver profile.
+        """
         payload = {'custid': custID}
         url = ct.URL_YEARLY_STATS
         response = await self.build_request(url, payload)
