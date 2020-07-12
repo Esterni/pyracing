6e05974d3eb468523f0c625363379c0ca0e5ab7d
Jacob Anderson <>
2020-07-12 14:24:30 -0700

    Removed redundent escape characters in the description. Since it is within brackets (), escape characters are not necessary

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setuptools.setup(
       name='pyracing',
       version='0.1.0',
       author='Jacob Anderson & Xander Riga',
-      description='A complete overhaul of ir_webstats; pyracing is an API'\
-        'wrapper for simracing service "iRacing" that queries known URL'\
-        'endpoints and returns JSON data that is accessible through objects'\
-        'instead of dictionaries.',
+      description='A complete overhaul of ir_webstats; pyracing is an API'
+      'wrapper for simracing service "iRacing" that queries known URL'
+      'endpoints and returns JSON data that is accessible through objects'
+      'instead of dictionaries.',
       long_description=long_description,
       long_description_content_type="text/markdown",
       url='https://github.com/Esterni/pyracing',
