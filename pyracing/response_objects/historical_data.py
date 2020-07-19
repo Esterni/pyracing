4b9d2f7d654597af3a8421ce241c59308d876a3f
esterni <Jaander@wvi.com>
2020-07-18 17:45:27 -0700

    modification of name order to `noun_adjective` for clarity

--- pyracing/response_objects/historical_data.py
+++ pyracing/response_objects/historical_data.py
@@ -3,9 +3,9 @@ from ..constants import parse_encode
 
 class EventResults:
     def __init__(self, dict):
-        self.best_qlap = parse_encode(dict['38'])
-        self.subsession_best_lap = parse_encode(dict['6'])
-        self.best_lap = dict['29']
+        self.qlap_best = parse_encode(dict['38'])
+        self.lap_best_subsession = parse_encode(dict['6'])
+        self.lap_best = dict['29']
         self.car_class_id = dict['32']
         self.car_id = dict['33']
         self.cat_id = dict['40']
@@ -17,11 +17,11 @@ class EventResults:
         self.display_name = dict['17']
         self.drop_race = dict['37']
         self.event_type = dict['10']
-        self.finish_pos = dict['3']
+        self.pos_finish = dict['3']
         self.group_name = dict['19']
-        self.helm_color1 = dict['1']
-        self.helm_color2 = parse_encode(dict['14'])
-        self.helm_color3 = parse_encode(dict['13'])
+        self.helm_color_1 = dict['1']
+        self.helm_color_2 = parse_encode(dict['14'])
+        self.helm_color_3 = parse_encode(dict['13'])
         self.helm_license_level = dict['24']
         self.helm_pattern = dict['46']
         self.incidents = dict['39']
@@ -36,16 +36,16 @@ class EventResults:
         self.session_id = dict['20']
         self.session_rank = dict['28']
         self.start_date = dict['9']
-        self.start_pos = parse_encode(dict['44'])
+        self.pos_start = parse_encode(dict['44'])
         self.start_time = parse_encode(dict['21'])
         self.strength_of_field = dict['12']
         self.subsession_id = dict['45']
         self.time_finished = parse_encode(dict['7'])
         self.track_id = dict['41']
         self.winner_display_name = dict['30']
-        self.winner_helm_color1 = parse_encode(dict['31'])
-        self.winner_helm_color2 = parse_encode(dict['5'])
-        self.winner_helm_color3 = dict['2']
+        self.winner_helm_color_1 = parse_encode(dict['31'])
+        self.winner_helm_color_2 = parse_encode(dict['5'])
+        self.winner_helm_color_3 = dict['2']
         self.winner_helm_pattern = parse_encode(dict['4'])
         self.winner_license_level = dict['36']
         self.winner_group_id = dict['25']
@@ -56,8 +56,8 @@ class EventResults:
 
 class SeasonStandings:
     def __init__(self, dict):
-        self.avg_finish_pos = dict['9']
-        self.avg_start_pos = dict['8']
+        self.finish_pos_avg = dict['9']
+        self.start_pos_avg = dict['8']
         self.club_id = dict['6']
         self.club_name = parse_encode(dict['23'])
         self.country = parse_encode(dict['1'])
@@ -67,9 +67,9 @@ class SeasonStandings:
         self.display_name = parse_encode(dict['26'])
         self.division = dict['11']
         self.dropped = dict['5']
-        self.helm_color1 = parse_encode(dict['25'])
-        self.helm_color2 = parse_encode(dict['27'])
-        self.helm_color3 = parse_encode(dict['22'])
+        self.helm_color_1 = parse_encode(dict['25'])
+        self.helm_color_2 = parse_encode(dict['27'])
+        self.helm_color_3 = parse_encode(dict['22'])
         self.helm_pattern = dict['20']
         self.incidents = dict['13']
         self.irating = dict['3']
@@ -77,15 +77,15 @@ class SeasonStandings:
         self.laps_led = dict['24']
         self.max_license_level = dict['21']
         self.points = dict['10']
-        self.poles_total = dict['19']
+        self.poles = dict['19']
         self.pos = dict['30']
         self.rank = dict['14']
         self.row = dict['31']
-        self.starts_total = dict['15']
+        self.starts = dict['15']
         self.sub_level = parse_encode(dict['29'])
         self.top_five = dict['12']
         self.week = dict['2']
-        self.win_total = dict['17']
+        self.wins = dict['17']
 
         # Used only on webpage to highlight the member in relaton to others
         # self.custrow = dict['16']
@@ -94,11 +94,11 @@ class SeasonStandings:
 
 class DriverStats:
     def __init__(self, dict):
-        self.avg_field_size = dict['13']
-        self.avg_finish_pos = dict['9']
-        self.avg_start_pos = dict['8']
-        self.avg_incidents = dict['26']
-        self.avg_points = dict['27']
+        self.field_size_avg = dict['13']
+        self.finish_pos_avg = dict['9']
+        self.start_pos_avg = dict['8']
+        self.incidents_avg = dict['26']
+        self.points_avg = dict['27']
         self.club_id = dict['5']
         self.club_name = parse_encode(dict['29'])
         self.club_points = dict['21']
@@ -107,9 +107,9 @@ class DriverStats:
         self.display_name = parse_encode(dict['32'])
         self.group_letter = parse_encode(dict['39'])
         self.group_name = parse_encode(dict['24'])
-        self.helm_color1 = parse_encode(dict['31'])
-        self.helm_color2 = parse_encode(dict['33'])
-        self.helm_color3 = parse_encode(dict['28'])
+        self.helm_color_1 = parse_encode(dict['31'])
+        self.helm_color_2 = parse_encode(dict['33'])
+        self.helm_color_3 = parse_encode(dict['28'])
         self.helm_face_type = dict['36']
         self.helm_helmet_type = dict['10']
         self.helm_pattern = dict['22']
@@ -119,18 +119,18 @@ class DriverStats:
         self.laps_led = dict['30']
         self.license_class = parse_encode(dict['16'])
         self.license_class_rank = dict['19']
-        self.license_class = dict['12']
+        self.license_class_id = dict['12']
         self.license_level = dict['23']
         self.points = dict['11']
         self.rank = dict['14']
         self.region = parse_encode(dict['38'])
         self.row = dict['37']
-        self.starts_total = dict['15']
+        self.starts = dict['15']
         self.sub_level = dict['35']
         self.top_25_percent = dict['1']
         self.ttrating = dict['25']
         self.ttrating_rank = dict['6']
-        self.win_total = dict['18']
+        self.wins = dict['18']
         # These are only used in front end webpage; Not in dict results
         # self.custrow = dict['17']
         # self.rowcount = dict['20']
@@ -146,13 +146,13 @@ class WorldRecords:
         self.country_code = parse_encode(dict['6'])
         self.cust_id = dict['31']
         self.display_name = parse_encode(dict['29'])
-        self.helm_color1 = parse_encode(dict['28'])
-        self.helm_color2 = parse_encode(dict['30'])
-        self.helm_color3 = parse_encode(dict['26'])
+        self.helm_color_1 = parse_encode(dict['28'])
+        self.helm_color_2 = parse_encode(dict['30'])
+        self.helm_color_3 = parse_encode(dict['26'])
         self.helm_pattern = dict['22']
         self.irating = dict['4']
         self.license_class = parse_encode(dict['3'])
-        self.license_class = dict['14']
+        self.license_class_id = dict['14']
         self.license_level = dict['23']
         self.practice = parse_encode(dict['2'])
         self.practice_start_time = dict['8']
@@ -169,8 +169,8 @@ class WorldRecords:
         self.season_year = parse_encode(dict['17'])
         self.sub_level = dict['32']
         self.timetrial = parse_encode(dict['37'])
-        self.timetrial_start_time = dict['25']
-        self.timetrial_subsession_id = ['1']
+        self.tt_start_time = dict['25']
+        self.tt_subsession_id = ['1']
         self.track_id = dict['5']
         self.ttrating = dict['24']
         # Data not used in the 'r' dictionary. Only for webpage display.
@@ -180,12 +180,12 @@ class WorldRecords:
 
 class PrivateResults:
     def __init__(self, dict):
-        self.best_lap = dict['bestlaptime']
+        self.lap_best = dict['bestlaptime']
         self.car_class_id = dict['carclassid']
         self.car_ids = dict['carids']
         self.cat_id = dict['catid']
-        self.class_finish_pos = dict['classfinishingposition']
-        self.class_start_pos = dict['classstartingposition']
+        self.finish_pos_class = dict['classfinishingposition']
+        self.start_pos_class = dict['classstartingposition']
         self.created = dict['created']
         self.finish_pos = dict['finishingposition']
         self.fixed_setup = dict['fixed_setup']
@@ -193,49 +193,49 @@ class PrivateResults:
         self.hardcore_level = dict['hardcorelevel']
         self.host_cust_id = dict['host_custid']
         self.host_display_name = dict['host_displayname']
-        self.host_helmet_color1 = dict['host_helmet_color1']
-        self.host_helmet_color2 = dict['host_helmet_color2']
-        self.host_helmet_color3 = dict['host_helmet_color3']
+        self.host_helmet_color_1 = dict['host_helmet_color1']
+        self.host_helmet_color_2 = dict['host_helmet_color2']
+        self.host_helmet_color_3 = dict['host_helmet_color3']
         self.host_helmet_face_type = dict['host_helmet_facetype']
         self.host_helmet_helmet_type = dict['host_helmet_helmettype']
         self.host_helmet_pattern = dict['host_helmet_pattern']
         self.host_license_level = dict['host_licenselevel']
         self.incidents = dict['incidents']
         self.lonequalify = dict['lonequalify']
-        self.max_pct_fuel_fills = dict['max_pct_fuel_fills']
-        self.max_drivers = dict['maxdrivers']
-        self.max_ir = dict['maxir']
-        self.max_lic_level = dict['maxliclevel']
-        self.min_ir = dict['minir']
-        self.min_lic_level = dict['minliclevel']
+        self.pct_fuel_fills_max = dict['max_pct_fuel_fills']
+        self.drivers_max = dict['maxdrivers']
+        self.ir_max = dict['maxir']
+        self.lic_level_max = dict['maxliclevel']
+        self.ir_min = dict['minir']
+        self.lic_level_min = dict['minliclevel']
         self.multiclass = dict['multiclass']
-        self.num_fast_tows = dict['numfasttows']
+        self.fast_tows_num = dict['numfasttows']
         self.practice_length = dict['practicelength']
         self.private = dict['private']
-        self.private_session_id = dict['privatesessionid']
-        self.qualify_laps = dict['qualifylaps']
-        self.qualify_length = dict['qualifylength']
-        self.qual_setup_filenames = dict['qualsetupfilenames']
-        self.qual_setup_ids = dict['qualsetupids']
-        self.race_finished_at = dict['racefinishedat']
-        self.race_laps = dict['racelaps']
-        self.race_length = dict['racelength']
-        self.race_setup_filenames = dict['racesetupfilenames']
-        self.race_setup_ids = dict['racesetupids']
+        self.session_id_private = dict['privatesessionid']
+        self.laps_qualify = dict['qualifylaps']
+        self.length_qualify = dict['qualifylength']
+        self.setup_filenames_qual = dict['qualsetupfilenames']
+        self.setup_ids_qual = dict['qualsetupids']
+        self.race_time_finished = dict['racefinishedat']
+        self.laps_race = dict['racelaps']
+        self.length_race = dict['racelength']
+        self.setup_filenames_race = dict['racesetupfilenames']
+        self.setup_ids_race = dict['racesetupids']
         self.restarts = dict['restarts']
         self.row = dict['rn']
         self.rolling_starts = dict['rollingstarts']
         self.session_fast_lap = dict['sessionfastlap']
         self.session_id = dict['sessionid']
         self.session_name = dict['sessionname']
-        self.start_time = dict['start_time']
-        self.start_pos = dict['startingposition']
-        self.subsession_finished_at = dict['subsessionfinishedat']
+        self.time_start = dict['start_time']
+        self.pos_start = dict['startingposition']
+        self.subsession_time_finished = dict['subsessionfinishedat']
         self.subsession_id = dict['subsessionid']
         self.time_of_day = dict['timeofday']
         self.track = dict['track_name']
         self.track_id = dict['trackid']
-        self.weather_fog_density = dict['weather_fog_density']
+        self.fog_density = dict['weather_fog_density']
         self.humidity = dict['weather_rh']
         self.skies = dict['weather_skies']
         self.temp_unit = dict['weather_temp_units']
@@ -247,12 +247,12 @@ class PrivateResults:
         self.weight_penalties = dict['weight_penalties']
         self.winner_display_name = dict['winner_displayname']
         self.winner_display_names = dict['winner_displaynames']
-        self.winner_helmet_color1 = dict['winner_helmet_color1']
-        self.winner_helmet_color2 = dict['winner_helmet_color2']
-        self.winner_helmet_color3 = dict['winner_helmet_color3']
+        self.winner_helmet_color_1 = dict['winner_helmet_color1']
+        self.winner_helmet_color_2 = dict['winner_helmet_color2']
+        self.winner_helmet_color_3 = dict['winner_helmet_color3']
         self.winner_helmet_pattern = dict['winner_helmet_pattern']
         self.winner_license_level = dict['winner_licenselevel']
-        self.winners_group_id = dict['winnersgroupid']
+        self.winner_group_id = dict['winnersgroupid']
 
 
 class SeriesRaceResults:
@@ -261,7 +261,7 @@ class SeriesRaceResults:
         self.official = dict['6']
         self.session_id = dict['4']
         self.size_of_field = dict['7']
-        self.start_time = dict['1']
+        self.time_start = dict['1']
         self.strength_of_field = dict['8']
         self.subsession_id = dict['5']
         self.track_id = dict['3']
@@ -269,29 +269,29 @@ class SeriesRaceResults:
 
 class Team:
     def __init__(self, dict):
-        self.avg_class_finish_pos = dict['avgclassfinishingposition']
-        self.avg_classsize_of_field = dict['avgclasssizeoffield']
-        self.avg_class_start_pos = dict['avgclassstartingposition']
-        self.avg_class_strength_of_field = dict['avgclassstrengthoffield']
-        self.avg_event_finish_pos = dict['avgeventfinishingposition']
-        self.avg_event_size_of_field = dict['avgeventsizeoffield']
-        self.avg_event_start_pos = dict['avgeventstartingposition']
-        self.avg_event_strength_of_field = dict['avgeventstrengthoffield']
+        self.finish_pos_class_avg = dict['avgclassfinishingposition']
+        self.size_of_field_class_avg = dict['avgclasssizeoffield']
+        self.start_pos_class_avg = dict['avgclassstartingposition']
+        self.strength_of_field_class_avg = dict['avgclassstrengthoffield']
+        self.finish_pos_event_avg = dict['avgeventfinishingposition']
+        self.size_of_field_event_avg = dict['avgeventsizeoffield']
+        self.start_pos_event_avg = dict['avgeventstartingposition']
+        self.strength_of_field_event_avg = dict['avgeventstrengthoffield']
         self.cars = dict['cars']
-        self.class_poles_total = dict['classpoles']
-        self.class_top_10_pcnt = dict['classtop10pcnt']
-        self.class_top_25_percent = dict['classtop25pcnt']
-        self.class_top_five = dict['classtopfive']
-        self.class_win_total = dict['classwins']
-        self.color1 = dict['color1']
-        self.color2 = dict['color2']
-        self.color3 = dict['color3']
+        self.poles_class = dict['classpoles']
+        self.top_10_pcnt_class = dict['classtop10pcnt']
+        self.top_25_percent_class = dict['classtop25pcnt']
+        self.top_five_class = dict['classtopfive']
+        self.wins_class = dict['classwins']
+        self.color_1 = dict['color1']
+        self.color_2 = dict['color2']
+        self.color_3 = dict['color3']
         self.cust_id = dict['custid']
-        self.event_poles_total = dict['eventpoles']
-        self.event_top_10_pcnt = dict['eventtop10pcnt']
-        self.event_top_25_percent = dict['eventtop25pcnt']
-        self.event_top_five = dict['eventtopfive']
-        self.event_win_total = dict['eventwins']
+        self.poles_event = dict['eventpoles']
+        self.top_10_pcnt_event = dict['eventtop10pcnt']
+        self.top_25_pcnt_event = dict['eventtop25pcnt']
+        self.top_five_event = dict['eventtopfive']
+        self.wins_event = dict['eventwins']
         self.face_type = dict['facetype']
         self.helmet_type = dict['helmettype']
         self.incidents = dict['incidents']
