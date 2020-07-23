9e05acec728d6133e369fafed90c1ec6e79e0f23
David Röhrig <dj@nohedidnt.de>
2020-07-23 14:53:27 +0200

    updated datetime helper function with input check

--- pyracing/helpers.py
+++ pyracing/helpers.py
@@ -7,7 +7,10 @@ from datetime import datetime
 
 # iRacing has all of their timestamps in ms so we need to divide
 def datetime_from_iracing_timestamp(timestamp):
-    return datetime.utcfromtimestamp(int(timestamp) / 1000)
+    if type(timestamp) is int or type(timestamp) is float:
+        return datetime.utcfromtimestamp(int(timestamp) / 1000)
+    else:
+        return None
 
 
 def default_logger():
