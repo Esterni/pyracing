9e940dab6e1651b743f00a1c5fa9004c96861345
Jacob Anderson <Esterni@users.noreply.github.com>
2020-07-23 20:29:18 -0700

    Update pyracing/helpers.py
    Co-authored-by: NoHeDidn't <dj@nohedidnt.de>

--- pyracing/helpers.py
+++ pyracing/helpers.py
@@ -31,6 +31,6 @@ def now_five_min_floor():
 
 def parse_encode(string):
     if not type(string) is str:
-        return None
+        return ''
     
     return urllib.parse.unquote(string).replace('+', ' ')
