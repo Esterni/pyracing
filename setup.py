5653471018ba563c58ff3c24c8b4a8e22634a4f8
Mike Holler <mike.holler@physiq.com>
2020-07-29 09:28:00 -0500

    Reformat setup.py for pep8 compliance

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ setuptools.setup(
     name='pyracing',
     version='0.1.0',
     author='Jacob Anderson & Xander Riga',
-    description='A complete overhaul of ir_webstats; pyracing is an API'
-    ' wrapper for simracing service "iRacing" that queries known URL'
-    ' endpoints and returns JSON data that is accessible through objects'
-    ' instead of dictionaries.',
+    description='A complete overhaul of ir_webstats; pyracing is an API '
+                'wrapper for simracing service "iRacing" that queries known '
+                'URL endpoints and returns JSON data that is accessible '
+                'through objects instead of dictionaries.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Esterni/pyracing',
@@ -25,4 +25,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     zip_safe=False,
-    install_requires=['httpx>=0.13.3,<0.14'])
+    install_requires=['httpx>=0.13.3,<0.14']
+)
