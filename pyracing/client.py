9440773acf3d4e90f744cc7ac5a84ab7d4b0b3db
Nathan <nathancrosty@gmail.com>
2022-04-05 10:07:00 -0400

    added some error checking for empty rows in get season helper func

--- pyracing/client.py
+++ pyracing/client.py
@@ -959,8 +959,10 @@ class Client:
         mSite = 'https://members.iracing.com/membersite/member'
         url = (mSite + '/GetLeagueCalendarBySeason')
         web_fb = await self._build_request(url, payload)
-        #return web_fb.json()["rows"]
-        return web_fb.json()
+        try:
+            return web_fb.json()["rows"]
+        except:
+            return web_fb.json()
 
 
 
